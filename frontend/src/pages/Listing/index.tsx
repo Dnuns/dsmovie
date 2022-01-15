b32e82f396490c041cf588cbcad21141a2187a0e
David Nunes <51808846+Dnuns@users.noreply.github.com>
2022-01-15 20:52:46 -0100

    remove setSourceMapRange

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -3,7 +3,6 @@ import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { MoviePage } from "types/movie";
-import { setSourceMapRange } from "typescript";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
