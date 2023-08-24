2f5310604345fb4a77b0e2e5ae99b73526a8091d
David Nunes <51808846+Dnuns@users.noreply.github.com>
2023-08-24 14:52:45 -0100

    add import.sql in src/test

---
+++ backend/src/test/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_user(email) VALUES ('maria.123@gmail.com');
+
+INSERT INTO tb_movie(score, count, title, image) VALUES (0, 0, 'O Rei Macaco', 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/bmwK5QCznqUT8bcDR7qROrxqgov.jpg');
+
+INSERT INTO tb_score(movie_id, user_id, value) VALUES (1, 1, 5.0);
