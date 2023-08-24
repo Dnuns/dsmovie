436fb69d4aec8be8a378665eb8308024a2790e11
David Nunes <51808846+Dnuns@users.noreply.github.com>
2023-08-24 12:34:22 -0100

    add UserRepositoryTest

---
+++ backend/src/test/java/com/devsuperior/dsmovie/repositories/UserRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dsmovie.repositories;
+
+import com.devsuperior.dsmovie.entities.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@TestPropertySource("/application-test.properties")
+@SpringBootTest
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository repository;
+
+    @Test
+    void findByEmail(){
+        User user = repository.findByEmail("maria@gmail.com");
+
+        assertNotNull(user);
+        assertEquals("maria@gmail.com",user.getEmail());
+    }
+}
