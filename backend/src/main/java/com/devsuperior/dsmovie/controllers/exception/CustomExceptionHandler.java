8ce256ff6f6f6aed16bc356e3d8957ef46769ab1
David Nunes <51808846+Dnuns@users.noreply.github.com>
2023-09-05 13:23:10 -0100

    Update CustomExceptionHandler.java

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/exception/CustomExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/exception/CustomExceptionHandler.java
@@ -3,7 +3,6 @@ package com.devsuperior.dsmovie.controllers.exception;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
