7e686ca7294be14aceada75b1c4ba8bac893d408
David Nunes <51808846+Dnuns@users.noreply.github.com>
2023-09-11 17:17:15 -0100

    Update UserController.java

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/UserController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/UserController.java
@@ -1,10 +1,8 @@
 package com.devsuperior.dsmovie.controllers;
 
 import com.devsuperior.dsmovie.dto.UserDTO;
-import com.devsuperior.dsmovie.entities.User;
 import com.devsuperior.dsmovie.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +14,7 @@ import javax.validation.Valid;
 @RequestMapping(value = "/users")
 @Controller
 public class UserController {
+
     @Autowired
     private UserService service;
 
