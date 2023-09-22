63e75bb5401c1010d92e910a19cd9c9bc9890a62
David Nunes <51808846+Dnuns@users.noreply.github.com>
2023-09-22 19:23:01 -0100

    Update Movie.java

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -10,6 +10,13 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "tb_movie")
 public class Movie {
@@ -25,63 +32,4 @@ public class Movie {
 	@OneToMany(mappedBy = "id.movie")
 	private Set<Score> scores = new HashSet<>();
 	
-	public Movie() {
-		
-	}
-
-	public Movie(Long id, String title, Double score, Integer count, String imgage) {
-		this.id = id;
-		this.title = title;
-		this.score = score;
-		this.count = count;
-		this.image = imgage;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public Double getScore() {
-		return score;
-	}
-
-	public void setScore(Double score) {
-		this.score = score;
-	}
-
-	public Integer getCount() {
-		return count;
-	}
-
-	public void setCount(Integer count) {
-		this.count = count;
-	}
-
-	public String getImgage() {
-		return image;
-	}
-
-	public void setImgage(String imgage) {
-		this.image = imgage;
-	}
-	
-	public Set<Score> getScores() {
-		return scores;
-	}
-	
-	public void setScores(Set<Score> scores) {
-		this.scores = scores;
-	}
 }
