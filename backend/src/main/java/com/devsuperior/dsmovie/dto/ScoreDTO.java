8d41ecfd231ff49a28e033b969d476c04ff289c6
David Nunes <51808846+Dnuns@users.noreply.github.com>
2023-09-22 19:22:54 -0100

    Update ScoreDTO.java

--- backend/src/main/java/com/devsuperior/dsmovie/dto/ScoreDTO.java
+++ backend/src/main/java/com/devsuperior/dsmovie/dto/ScoreDTO.java
@@ -1,35 +1,16 @@
 package com.devsuperior.dsmovie.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ScoreDTO {
 	
 	private Long movieId;
 	private String email;
 	private Double score;
 	
-	public ScoreDTO() {
-	}
-
-	public Long getMovieId() {
-		return movieId;
-	}
-
-	public void setMovieId(Long movieId) {
-		this.movieId = movieId;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Double getScore() {
-		return score;
-	}
-
-	public void setScore(Double score) {
-		this.score = score;
-	}
 }
