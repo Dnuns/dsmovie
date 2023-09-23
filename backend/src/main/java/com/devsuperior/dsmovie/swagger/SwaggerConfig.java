0177fd1eab072b0c56d66ad81578bcc410158801
David Nunes <51808846+Dnuns@users.noreply.github.com>
2023-09-23 18:32:56 -0100

    Update SwaggerConfig.java

--- backend/src/main/java/com/devsuperior/dsmovie/swagger/SwaggerConfig.java
+++ backend/src/main/java/com/devsuperior/dsmovie/swagger/SwaggerConfig.java
@@ -27,10 +27,10 @@ public class SwaggerConfig implements WebMvcConfigurer {
     private ApiInfo apiInfoMetaData() {
 
         return new ApiInfoBuilder().title("DSMOVIE")
-                .description("API Endpoint Decoration")
-                .contact(new Contact("Dev-Team", "https://www.dev-team.com/", "dev-team@gmail.com"))
-                .license("Apache 2.0")
-                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .description("API Endpoint")
+                .contact(new Contact("David Nunes", "https://github.com/Dnuns", "davsnuns@gmail.com"))
+                .license("The MIT License (MIT)")
+                .licenseUrl("https://mit-license.org")
                 .version("1.0.0")
                 .build();
     }
