529e2d7c33dd11e5673c9504fe4f2f80f096824e
David Nunes <51808846+Dnuns@users.noreply.github.com>
2023-10-09 22:18:25 -0100

    Update UserRepositoryTest.java

--- backend/src/test/java/com/devsuperior/dsmovie/repositories/UserRepositoryTest.java
+++ backend/src/test/java/com/devsuperior/dsmovie/repositories/UserRepositoryTest.java
@@ -1,13 +1,18 @@
 package com.devsuperior.dsmovie.repositories;
 
 import com.devsuperior.dsmovie.entities.User;
+import com.devsuperior.dsmovie.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Optional;
+
+import static com.devsuperior.dsmovie.utils.Role.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @TestPropertySource("/application-test.properties")
@@ -17,11 +22,37 @@ class UserRepositoryTest {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private UserService service;
+
     @Test
-    void findByEmailShouldReturnUser(){
+    void findByEmailShouldReturnUserWhenEmailExists(){
+        
         User user = repository.findByEmail("maria.123@gmail.com");
 
         assertNotNull(user.getEmail());
         assertEquals("maria.123@gmail.com",user.getEmail());
     }
+
+    @Test
+    void findByEmailShouldReturnNullWhenEmailDoesNotExist(){
+
+        User user = repository.findByEmail("bertelano@gmail.com");
+
+        assertNull(user);
+    }
+
+//    @Transactional
+//    @Test
+//    void updateUserRoleShouldUpdateRoleWhenDifferent(){
+//
+//        Optional<User> maria = repository.findById(1L);
+//
+//        assertEquals(ADMIN, maria.get().getRole());
+//
+//        repository.updateUserRole(USER, "maria.123@gmail.com");
+//
+//        assertEquals(USER, maria.get().getRole());
+//    }
+
 }
