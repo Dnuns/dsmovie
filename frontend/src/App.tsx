2b839149a01cffe6f8aed3c1ae6d75994cf181d3
David Nunes <51808846+Dnuns@users.noreply.github.com>
2023-10-31 23:05:05 -0100

    add more route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import Listing from 'pages/Listing';
-import Form from 'pages/Form';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Listing from "pages/Listing";
+import Form from "pages/Form";
 import Navbar from "components/Navbar";
+import LoginPage from "pages/Login";
+import RegisterPage from "pages/Register";
+import ProfilePage from "pages/Profile";
+import AdminPage from "pages/Admin";
+import NotFoundPage from "pages/NotFound";
+import UnauthorizedPage from "pages/Unauthorized";
 
 function App() {
   return (
@@ -16,6 +18,13 @@ function App() {
         <Route path="/form">
           <Route path=":movieId" element={<Form />} />
         </Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/404" element={<NotFoundPage />} />
+        <Route path="/401" element={<UnauthorizedPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,23 +1,41 @@
-import { ReactComponent as GithubIcon } from 'assets/img/github.svg'
-import './styles.css'
+import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import "./styles.css";
 
 function Navbar() {
-
-    return (
-        <header>
-            <nav className="container">
-                <div className="dsmovie-nav-content">
-                    <h1>DSMovie</h1>
-                    <a href="https://github.com/Dnuns" target="_blank" rel="noreferrer">
-                        <div className="dsmovie-contact-container">
-                            <GithubIcon />
-                            <p className="dsmovie-contact-link">/Dnuns</p>
-                        </div>
-                    </a>
-                </div>
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <span className="dsmovie-menu-logo">
+            <span className="dsmovie-menu">
+              <li>
+                <a href="/">
+                  <h1 className="home">DSmovie</h1>
+                </a>
+              </li>
+              <li>
+                <a href="/admin">Admin</a>
+              </li>
+            </span>
+          </span>
+          {/* <a href="https://github.com/Dnuns" target="_blank" rel="noreferrer">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/Dnuns</p>
+            </div>
+          </a> */}
+          <span className="dsmovie-menu">
+            <li>
+              <a href="/register">Sign Up</a>
+            </li>
+            <li>
+              <a href="/login">Sign In</a>
+            </li>
+          </span>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
